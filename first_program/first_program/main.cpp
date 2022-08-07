b2ea8d0bfed10a2c2b2afed6fbd38d82770c4fa0
mucahitdemir <mucahitdemirc@gmail.com>
2022-08-07 18:09:13 +0300

    fundamental steps are implemented

--- first_program/first_program/main.cpp
+++ first_program/first_program/main.cpp
@@ -1,14 +1,70 @@
 #include <iostream>
 
+using namespace std;
+
 int main()
 {
-	std::cout<<"Hello world"<<std::endl;
+//	std::cout<<"Hello world"<<std::endl;
+//    
+//    int favorite_number;
+//    
+//    std::cout<<"Enter your favorite number from 1-100:";
+//    std::cin >> favorite_number;
+//    std::cout <<"Amazing! that's my favorite number!"<< std::endl;
     
-    int favorite_number;
+       // cout << "Hello world!";
+//    int num1;
+//    int num2;
+//    double num3;
+
+//    cout << "Hello";
+//    cout << "World" << endl;
+
+//    cout << "Hello world!" << endl;
+//    cout << "Hello" << " world!" << endl;
+//    cout << "Hello" << " world!\n";
+//    cout << "Hello\nOut\nThere\n";
+
+//    int num1;
+//    int num2;
+//    double num3;
+
+//    cout <<  "Enter an integer: ";
+//    cin >> num1;
+//    cout << "You entered: " << num1 << endl;
+
+//    cout << "Enter a first integer: ";
+//    cin >> num1;
+//    
+//    cout << "Enter a second integer: ";
+//    cin >> num2;
+//    
+//    cout << "You entered " << num1 << " and " << num2 << endl;
     
-    std::cout<<"Enter your favorite number from 1-100:";
-    std::cin >> favorite_number;
-    std::cout <<"Amazing! that's my favorite number!"<< std::endl;
     
+//    cout << "Enter 2 integers separated with a space: ";
+//    cin >> num1 >> num2;
+//    cout << "You entered " << num1 << " and " << num2 << endl;
+
+
+
+//    cout << "Enter a double: ";
+//    cin >> num3;
+//    
+//    cout << "You entered : " << num3 << endl;
+
+   
+//    cout << "Enter an integer: ";
+//    cin >> num1;
+//    
+//    cout << "Enter a double: ";
+//    cin >> num3;
+//    
+//    cout << "The integer is: " << num1 << endl;
+//    cout << "And the double is " << num3 << endl;
+  
+    cout << "float: " << sizeof(float) << endl; // float: 4
+    cout << "double: " << sizeof(double) << endl;// double: 8
+
 	return 0;
 }
