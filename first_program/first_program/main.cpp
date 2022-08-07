38a0f2d61c4eac3f02accdf2ceb0cc30dd629172
mucahitdemir <mucahitdemirc@gmail.com>
2022-08-07 11:03:12 +0300

    started learning C++

---
+++ first_program/first_program/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+	std::cout<<"Hello world"<<std::endl;
+    
+    int favorite_number;
+    
+    std::cout<<"Enter your favorite number from 1-100:";
+    std::cin >> favorite_number;
+    std::cout <<"Amazing! that's my favorite number!"<< std::endl;
+    
+	return 0;
+}
