5e81edd6dbf9e177e2763ca0fa63b278d325fc54
Mücahit DEMİRCİ <mucahit.demirci@aspilsan.local>
2022-08-15 08:59:21 +0300

    Section 8: Operators and Statements challenge is solved

--- first_program/first_program/main.cpp
+++ first_program/first_program/main.cpp
@@ -75,39 +75,96 @@ int main()
 //    cin >> y;
 //    sum = x + y;
 //    cout << "Sum is: " << sum;
+//
+///**Character Type**/
+//
+//    char middle_initial{'A'};
+//    cout << "My middle initial is "<< middle_initial << endl;
+//    
+//
+///* Integer Type*****/
+//    unsigned short int my_exam_score{87};
+//    cout << "My exam score is "<< my_exam_score << endl;
+//    int country_number{65};
+//    cout << "I know more than "<< country_number <<" country names"<< endl;
+//    
+//    long istanbul_population{20'000'000};
+//    cout << "The population of Istanbul is around "<< istanbul_population << " millions" << endl;
+//    
+//    long long Alpha_centauri_distance{9'461'000'000'000};
+//    cout << "The distance for the Alpha Centauri is " << Alpha_centauri_distance <<" km" << endl;
+//    
+///****Floating Point Types****/
+//    float discount{15.5};
+//    cout <<"Discount is around "<< discount<< endl;
+//
+///*Sizeof variable*/
+//    cout << "Size of char: " << sizeof(char) << " byte" << endl;
+//    cout << "Size of int: " << sizeof(int) << " bytes" << endl;
+//    cout << "Size of float: " << sizeof(float) << " bytes" << endl;
+//    cout << "Size of double: " << sizeof(double) << " bytes" << endl;
+//    
+///*Minimum Values*/
+//    
+//    cout << "Minimum values: "<< endl;
+//    cout << "char: "<< CHAR_MIN << endl;
+//    cout << "int: "<< INT_MIN << endl;
 
-/**Character Type**/
 
-    char middle_initial{'A'};
-    cout << "My middle initial is "<< middle_initial << endl;
-    
 
-/* Integer Type*****/
-    unsigned short int my_exam_score{87};
-    cout << "My exam score is "<< my_exam_score << endl;
-    int country_number{65};
-    cout << "I know more than "<< country_number <<" country names"<< endl;
-    
-    long istanbul_population{20'000'000};
-    cout << "The population of Istanbul is around "<< istanbul_population << " millions" << endl;
-    
-    long long Alpha_centauri_distance{9'461'000'000'000};
-    cout << "The distance for the Alpha Centauri is " << Alpha_centauri_distance <<" km" << endl;
-    
-/****Floating Point Types****/
-    float discount{15.5};
-    cout <<"Discount is around "<< discount<< endl;
+/****************Section 8 Statements and Operators***************/
+//CHALLENGE 
 
-/*Sizeof variable*/
-    cout << "Size of char: " << sizeof(char) << " byte" << endl;
-    cout << "Size of int: " << sizeof(int) << " bytes" << endl;
-    cout << "Size of float: " << sizeof(float) << " bytes" << endl;
-    cout << "Size of double: " << sizeof(double) << " bytes" << endl;
-    
-/*Minimum Values*/
-    
-    cout << "Minimum values: "<< endl;
-    cout << "char: "<< CHAR_MIN << endl;
-    cout << "int: "<< INT_MIN << endl;
+const int dollar_value{100};
+const int quarter_value{25};
+const int dime_value{10};
+const int nickel_value{5};
+
+int entered_amount{};
+
+cout << "Enter an amount int cents: ";
+
+cin >> entered_amount;
+
+int balance{}, dollars{}, quarters{}, dimes{}, nickels{}, pennies{};
+
+/*SOLUTION WITHOUT USING MODULO OPERATOR*/
+
+//dollars = entered_amount / dollar_value;
+//balance =  entered_amount - (dollars * dollar_value);
+//
+//quarters = balance / quarter_value;
+//balance -= quarters * quarter_value;
+//
+//dimes = balance / dime_value;
+//balance -= dimes * dime_value;
+//
+//nickels = balance / nickel_value;
+//balance -= nickels * nickel_value;
+//
+//pennies = balance;
+
+/*MODULO*/
+
+dollars = entered_amount / dollar_value;
+balance =  entered_amount % dollar_value;
+
+quarters = balance / quarter_value;
+balance %= quarter_value;
+
+dimes = balance / dime_value;
+balance %= dime_value;
+
+nickels = balance / nickel_value;
+balance %= nickel_value;
+
+pennies = balance;
+
+cout << "Dollars: " << dollars << endl;
+cout << "Quarters: " << quarters << endl;
+cout << "Dimes: " << dimes << endl;
+cout << "Nickels: "<< nickels << endl;
+cout << "Pennies: "<< pennies << endl; 
+
 	return 0;
 }
